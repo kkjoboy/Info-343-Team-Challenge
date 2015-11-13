e8c4705179484b253c98b52aa580ff8297e7f37a
Ashlyn Opgrande <ashlyno@uw.edu>
2015-11-12 18:31:25 -0800

    fixes #7

--- js/app.js
+++ js/app.js
@@ -3,5 +3,8 @@
 angular.module('App', ['ngSanitize']) //ngSanitize for HTML displaying
 
 .controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
-
+	var startdate = new Date();
+	var daysToSubtract = 365.25*13-1;
+	startdate.setDate(startdate.getDate()-daysToSubtract);
+		$scope.youngest=startdate;
 }]);
