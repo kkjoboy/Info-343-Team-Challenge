791f621a5f8499968b15bda62fb92fda6cd5dd88
Joshua Mielke <joshuamielke@gmail.com>
2015-11-16 17:14:37 -0800

    closes #9

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ angular.module('App', ['ngSanitize']) //ngSanitize for HTML displaying
 	var startdate = new Date();
 	var daysToSubtract = 365.25*13-1;
 	startdate.setDate(startdate.getDate()-daysToSubtract);
-		$scope.youngest=startdate;
+	$scope.youngest = startdate;
 
 	$('#formSubmit').click(function() {
         $('.alert').show()
